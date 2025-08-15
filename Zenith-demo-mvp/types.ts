09218f13c118333195e1db8cbeacccad924c691b
P-Bao <BaoPV.B23KH006@stu.ptit.edu.vn>
2025-08-15 10:24:20 +0700

    Delete folder Zenith-demo-mvp

--- Zenith-demo-mvp/types.ts
+++
@@ -1,8 +0,0 @@
-export type Command =
-  | { action: 'SET_CALENDAR'; params?: { date?: string } }
-  | { action: 'SHOW_HEALTH_STATS' }
-
-export interface AIResponse {
-  reply: string
-  command?: Command | null
-}
